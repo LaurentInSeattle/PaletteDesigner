f5da48eaafd84348a646deff8adf74522ca4de3c
Laurent <ly.testud@outlook.com>
2025-07-20 12:29:19 -0700

    Fix for the circle adjustement method that was causing marker points to drift

--- Lyt.Avalonia.PaletteDesigner.Model/DataObjects/Position.cs
+++ Lyt.Avalonia.PaletteDesigner.Model/DataObjects/Position.cs
@@ -2,7 +2,7 @@
 
 // Screen position or a marker on the shades image:
 //      0,0 at top left, 300, 300 at bottom right
-//      defaults to center 
+//      defaults to center of the Shades Image circle 
 public sealed class Position(
     int x = PaletteDesignerModel.ShadesImageDimension / 2,
     int y = PaletteDesignerModel.ShadesImageDimension / 2)
@@ -11,21 +11,29 @@ public sealed class Position(
 
     public int Y { get; set; } = y;
 
+    // Constrain point to be located inside the Shades Image circle 
     public void Adjust()
     {
+        // normalize to center 
         double half = PaletteDesignerModel.ShadesImageDimension / 2.0;
         double x = (this.X - half) / half;
         double y = (half - this.Y) / half;
-        double radius = Math.Min(1.0, Math.Sqrt(x * x + y * y));
-        double angle = Math.Atan2(y, x);
-        x = radius * Math.Cos(angle);
-        y = radius * Math.Sin(angle);
-        int newX = (int)(x * half + half);
-        int newY = (int)(half - y * half);
-        newX = newX.Clip(PaletteDesignerModel.ShadesImageMax);
-        newY = newY.Clip(PaletteDesignerModel.ShadesImageMax);
-        this.X = newX;
-        this.Y = newY;
+        double radius = Math.Sqrt(x * x + y * y);
+        if (radius > 1.0)
+        {
+            // adjustment is needed
+            double minRadius = Math.Min(1.0, radius);
+            double angle = Math.Atan2(y, x);
+            x = minRadius * Math.Cos(angle);
+            y = minRadius * Math.Sin(angle);
+            int newX = (int)(0.5 + x * half + half);
+            int newY = (int)(0.5 + half - y * half);
+            newX = newX.Clip(PaletteDesignerModel.ShadesImageMax);
+            newY = newY.Clip(PaletteDesignerModel.ShadesImageMax);
+            this.X = newX;
+            this.Y = newY;
+        }
+        // else: No change needed
     }
 
     public Position MoveBy(Position position)
