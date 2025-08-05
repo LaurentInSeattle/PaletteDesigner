f3662861f18e6b8a196d80adede53c7741cbc535
Laurent <ly.testud@outlook.com>
2025-08-05 09:20:47 -0700

    Mappings: Showing test widgets

--- Lyt.Avalonia.PaletteDesigner/Workflow/Mapping/DraggableColorBoxView.axaml.cs
+++ Lyt.Avalonia.PaletteDesigner/Workflow/Mapping/DraggableColorBoxView.axaml.cs
@@ -2,12 +2,6 @@ namespace Lyt.Avalonia.PaletteDesigner.Workflow.Mapping;
 
 public partial class DraggableColorBoxView : View
 {
-    protected override void OnLoaded(object? sender, RoutedEventArgs e)
-    {
-        base.OnLoaded(sender, e);
-        Debug.WriteLine("DraggableColorBoxView : Loaded "); 
-    }
-
     protected override void OnDataContextChanged(object? sender, EventArgs e)
     {
         if (this.DataContext is DraggableColorBoxViewModel viewModel)
