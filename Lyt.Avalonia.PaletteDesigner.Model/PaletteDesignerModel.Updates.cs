5a9af03a878ed9c7d48714f2e7ea33cf64fbef46
Laurent <ly.testud@outlook.com>
2025-08-14 15:04:11 -0700

    Glyph button: faster continuous mode (will need a property at some point)

--- Lyt.Avalonia.PaletteDesigner.Model/PaletteDesignerModel.Updates.cs
+++ Lyt.Avalonia.PaletteDesigner.Model/PaletteDesignerModel.Updates.cs
@@ -4,10 +4,90 @@ using Lyt.Avalonia.PaletteDesigner.Model.PaletteObjects;
 
 public sealed partial class PaletteDesignerModel : ModelBase
 {
+    private bool suspendPaletteUpdates;
+    private Random random = new((int)DateTime.Now.Ticks);
+
+    private double RandomizeWheel(Palette palette)
+    {
+        double wheel = this.random.NextDouble() * 360.0;
+        int retries = 666;
+        while (retries > 0)
+        {
+            wheel = this.random.NextDouble() * 360.0;
+            if (Math.Abs(wheel - palette.Primary.Wheel) > 10.0 &&
+                Math.Abs(wheel - palette.Complementary.Wheel) > 10.0 &&
+                Math.Abs(wheel - palette.Secondary1.Wheel) > 10.0 &&
+                Math.Abs(wheel - palette.Secondary2.Wheel) > 10.0)
+            {
+                return wheel;
+            } 
+
+            --retries;
+            if (retries < 0)
+            {
+                return wheel;
+            }
+        }
+
+        return wheel;
+    }
+
+    private double RandomizeWheelDistance(Palette palette)
+    {
+        double distance = this.random.NextDouble() * 360.0;
+        int retries = 666;
+        while (retries > 0)
+        {
+            distance = this.random.NextDouble() * 360.0;
+            if (Math.Abs(distance - palette.SecondaryWheelDistance) > 10.0 && 
+                distance > 23.0 && 
+                distance < 180.0 - 23.0 )
+            {
+                return distance;
+            }
+
+            --retries;
+            if (retries < 0)
+            {
+                return distance;
+            }
+        }
+
+        return distance;
+    }
+
     public void RandomizePalette()
         => this.UpdatePalette(palette =>
         {
-            palette.Kind = PaletteKind.Quadrichromatic;
+            GeneralExtensions.With(ref this.suspendPaletteUpdates, () =>
+            {
+                try
+                {
+                    // Randomize a shade preset 
+                    var shadesPresets = this.ShadesPresets.Values.ToList();
+                    int index = this.random.Next(shadesPresets.Count);
+                    ShadesPreset shadesPreset = shadesPresets[index];
+                    shadesPreset = ShadesPreset.FromSizeIndependant(shadesPreset);
+                    palette.ApplyShadesPreset(shadesPreset);
+
+                    // Randomize a wheel distance for triadic 
+                    palette.SecondaryWheelDistance = this.RandomizeWheelDistance(palette);
+
+                    // Randomize all wheels, may throw 
+                    double primary = this.RandomizeWheel(palette);
+                    this.UpdatePalettePrimaryWheel(primary);
+                    double complementary = this.RandomizeWheel(palette);
+                    this.UpdatePaletteComplementaryWheel(complementary);
+                    double secondary1 = this.RandomizeWheel(palette);
+                    this.UpdatePaletteSecondary1Wheel(secondary1);
+                    double secondary2 = this.RandomizeWheel(palette);
+                    this.UpdatePaletteSecondary2Wheel(secondary2);
+                }
+                catch 
+                {
+                    // Swallow silently
+                }
+            });
 
             return true;
         });
@@ -55,7 +135,7 @@ public sealed partial class PaletteDesignerModel : ModelBase
     private bool UpdatePalette(Func<Palette, bool> action)
     {
         bool result = this.ActionPalette(action);
-        if (result)
+        if (result && !this.suspendPaletteUpdates)
         {
             this.Messenger.Publish(new ModelPaletteUpdatedMessage());
         }
