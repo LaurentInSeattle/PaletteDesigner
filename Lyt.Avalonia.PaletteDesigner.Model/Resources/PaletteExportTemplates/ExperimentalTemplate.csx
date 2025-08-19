c5a6525efed02ff3fb31e19429878427a1e2b4af
Laurent <ly.testud@outlook.com>
2025-08-19 11:32:16 -0700

    experiments

---
+++ Lyt.Avalonia.PaletteDesigner.Model/Resources/PaletteExportTemplates/ExperimentalTemplate.csx
@@ -0,0 +1,22 @@
+﻿#r "System"
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+var powers = Enumerable.Range(10, 5);
+
+$@"// Generation time: {DateTime.Now}
+
+namespace {Namespace}
+{{
+    public static class QuickMath
+    {{
+        {string.Join
+            (
+                Environment.NewLine,
+                powers.Select(i => $@"public static double TwoToThePowerOf{i} => {Math.Pow(2, i)};")
+            )}
+    }}
+}}
+"
---
+++ Lyt.Avalonia.PaletteDesigner.Model/Utilities/CsxTemplator.cs
@@ -0,0 +1,35 @@
+﻿namespace Lyt.Avalonia.PaletteDesigner.Model.Utilities;
+
+using Microsoft.CodeAnalysis.CSharp.Scripting;
+using Microsoft.CodeAnalysis.Scripting;
+
+public static class CsxTemplator
+{
+    public static async Task<string> Generate(string template, string variable)
+    {
+        try
+        {
+            var options = ScriptOptions.Default;
+            var script = 
+                CSharpScript
+                    .Create<string>($@"var Namespace = ""{variable}"";")
+                    .ContinueWith(template, options);
+            var state = await script.RunAsync();
+            if (state is not null && state.ReturnValue is not null)
+            {
+                string? generated = state.ReturnValue.ToString();
+                if (!string.IsNullOrWhiteSpace(generated))
+                {
+                    return generated;
+                }
+            }
+
+            return string.Empty;
+        }
+        catch  (Exception ex) 
+        { 
+            Debug.WriteLine(ex);
+            return string.Empty;
+        }
+    }
+}
