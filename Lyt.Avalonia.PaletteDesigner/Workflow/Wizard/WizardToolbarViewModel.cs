ac9528c330b265bbf9599a884c74c7ad3cade364
Laurent <ly.testud@outlook.com>
2026-01-30 16:03:11 -0800

    WIzard exports now complete - Wizard toolbar in progress (broken)

--- Lyt.Avalonia.PaletteDesigner/Workflow/Wizard/WizardToolbarViewModel.cs
+++ Lyt.Avalonia.PaletteDesigner/Workflow/Wizard/WizardToolbarViewModel.cs
@@ -4,48 +4,42 @@ public sealed partial class WizardToolbarViewModel : ViewModel<WizardToolbarView
 {
     private readonly PaletteDesignerModel paletteDesignerModel;
 
-    //[ObservableProperty]
-    //private bool isDeepAlgorithmStrength;
+    [ObservableProperty]
+    private bool showShadesValues;
 
-    //[ObservableProperty]
-    //private double clustersSliderValue;
-
-    //[ObservableProperty]
-    //private string clustersValue = string.Empty;
 
     public WizardToolbarViewModel()
     {
         this.paletteDesignerModel = App.GetRequiredService<PaletteDesignerModel>();
-        //this.IsDeepAlgorithmStrength = false;
-        //this.ClustersSliderValue = 10.0;
-        //this.UpdateSliderLabel();
     }
 
-    //partial void OnIsDeepAlgorithmStrengthChanged(bool value)
-    //    => this.paletteDesignerModel.IsDeepImagingAlgorithmStrength = value;
+    partial void OnShowShadesValuesChanged(bool value)
+        => new ShadesValuesVisibilityMessage(value).Publish();
 
-    //public void UpdateSliderLabel()
-    //    => this.ClustersValue = string.Format("{0:D}", (int)this.ClustersSliderValue);
 
-    //partial void OnClustersSliderValueChanged(double value)
-    //{
-    //    this.UpdateSliderLabel();
-    //    this.paletteDesignerModel.ImagingAlgorithmClusters = (int)value;
-    //}
-
-    //public void ProgrammaticUpdate(ColorSwatches swatches)
-    //{
-    //    this.IsDeepAlgorithmStrength = swatches.IsDeepAlgorithmStrength;
-    //    this.ClustersSliderValue = swatches.Swatches.Count;
-    //}
+#pragma warning disable CA1822  
+    // Mark members as static
+    // Relay commands cannot be static
 
     [RelayCommand]
-#pragma warning disable CA1822  // Mark members as static
-    // Relay commands cannot be static
+    public void OnDisplayMode(object? parameter)
+    {
+        if (parameter is string tag)
+        {
+            // Update model 
+            ShadesValuesDisplayMode mode =
+                Enum.TryParse(tag, out ShadesValuesDisplayMode kind) ? kind : ShadesValuesDisplayMode.Hex;
+            this.paletteDesignerModel.ShadesValuesDisplayMode = mode;
+            new ModelShadesDisplayModeUpdated().Publish();
+        }
+    }
+
+    [RelayCommand]
     public void OnRecalculate()
     {
         var viewModel = App.GetRequiredService<ImagingViewModel>();
         viewModel.ReProcessBitmap();
     }
+
 #pragma warning restore CA1822 // Mark members as static
 }
